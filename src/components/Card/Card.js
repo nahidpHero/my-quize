c114d59c1212603263d0fc06ce062ccbb81ab937
Nahidhassan1 <nahidlikhon22@gmail.com>
2022-10-11 10:03:03 +0600

    font awesome added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee,faEye } from '@fortawesome/free-solid-svg-icons'
 import React, { useState } from 'react';
 import Option from '../Options/Option';
 import './Card.css'
@@ -12,10 +14,13 @@ const Card = ({question}) => {
         console.log("Your ans is flase")
       }
    }
+   const showAnswre=(answre)=>{
+      console.log(answre)
+   }
     return (
         <div>
             <div className="question-card">
-                
+                <h1 onClick={()=>showAnswre(answre)} className='text-end m-4 eye text-2xl'><FontAwesomeIcon icon={faEye} ></FontAwesomeIcon></h1>
                 <h1 className='text-3xl font-semibold question-text'>Quize:{question.question}</h1>
                 {
                     options.map(option=><Option
