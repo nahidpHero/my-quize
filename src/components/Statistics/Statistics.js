c5648be227ace81f5d0a314fb88177cd06c3f56e
Nahidhassan1 <nahidlikhon22@gmail.com>
2022-10-11 16:40:18 +0600

    rechart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const Statistics = () => {
+    const datas=useLoaderData()
+    const data=datas.data;
+    console.log(data)
+
+
     return (
-        <div>
-            <h1>This is statistic</h1>
+        <div className='m-5'>
+            <ResponsiveContainer width={500} height={400}>
+            <BarChart width={500} height={400} data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total" fill="#8884d8" />
+            </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
